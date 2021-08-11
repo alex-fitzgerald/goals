54d15d8053d0044e5227feb929f6b4e9c9cce36c
Alex <fitzgerald.s.alexander@gmail.com>
2021-08-11 22:06:22 +1200

    install legacy-peer-deps

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ require('./database');
 app.use(bodyParser.json());
 app.use(cors());
 
-const kwmlgoals = require(path.join(__dirname, './api/kwmlgoals'));
-app.use(path.join(__dirname, './api/kwmlgoals'), kwmlgoals);
+const kwmlgoals = require(path.join(__dirname, './kwmlgoals'));
+app.use(path.join(__dirname, './kwmlgoals'), kwmlgoals);
 
 app.use(express.static(path.join(__dirname, '../build')))
 app.get('*', (req, res) => {
