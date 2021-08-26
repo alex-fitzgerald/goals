98f665617e978006f93671d009227863d7f044ca
Alex <fitzgerald.s.alexander@gmail.com>
2021-08-26 20:46:26 +1200

    Amend styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation(props){
             <button name="Mindsets" onClick={() => (props.handleClick("Mindsets"))}>Mindsets</button> 
             <button name="Reminders" onClick={() => (props.handleClick("Reminders"))}>Reminders</button> 
             <button name="Daily" onClick={() => (props.handleClick("Daily"))}>Daily Goals</button> 
-            <button name="LongTerm" onClick={() => (props.handleClick("LongTerm"))}>Long Term Goals</button> 
+            <button name="LongTerm" onClick={() => (props.handleClick("LongTerm"))}>Long Term</button> 
             <button name="AllGoals" onClick={() => (props.handleClick("AllGoals"))}>All Goals</button> 
             <button name="Create" onClick={() => (props.handleClick("Create"))}>Add</button> 
         </div>
